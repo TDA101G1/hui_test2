28042b8c4a401105eb714759681572cf0ccd8eba
sunset1117 <sunset1117@hotmail.com>
2020-06-28 04:31:49 +0800

    增加jedis.close();

--- src/com/customerize/websocket/controller/JedisEditSchedule.java
+++ src/com/customerize/websocket/controller/JedisEditSchedule.java
@@ -16,6 +16,7 @@ public class JedisEditSchedule {
 		jedis.auth("123456");
 		String key = new StringBuilder("member").append(":").append(member_id).toString();
 		jedis.sadd(key, cust_schedule_id);
+		jedis.close();
 	}
 	
 	public static void setRoomMembers (String cust_schedule_id, MemberDetail member) {
@@ -25,25 +26,48 @@ public class JedisEditSchedule {
 	String key = new StringBuilder("customerize").append(":").append(cust_schedule_id)
 			.append(":").append("room").toString();
 	jedis.sadd(key, gson.toJson(member));
+	jedis.close();
 }
 	
 	public static Set<MemberDetail> getRoomMembers (String cust_schedule_id) {
-		Set<MemberDetail> sets = null;
-		Gson gson = new Gson();
 		Jedis jedis = new Jedis("localhost", 6379);
 		jedis.auth("123456");
 		String key = new StringBuilder("customerize").append(":").append(cust_schedule_id)
 				.append(":").append("room").toString();
+		System.out.println(key);
 		Set<String> setString = jedis.smembers(key);
-		
-		if(setString != null || setString.size() > 0) {
-			sets = new HashSet<MemberDetail>();
-			for(String set : setString) {
-				sets.add(gson.fromJson(set, MemberDetail.class));
-			}
+		if(setString == null || setString.size() == 0) {
+			jedis.close();
+			return null;
 		}
+		Set<MemberDetail> sets = new HashSet<MemberDetail>();
+		for(String set : setString) {
+			sets.add(new Gson().fromJson(set, MemberDetail.class));
+		}
+		jedis.close();
 		return sets;
 	}
+	
+//	public static Set<MemberDetail> getRoomMembers (String cust_schedule_id) {
+//		Gson gson = new Gson();
+//		Jedis jedis = new Jedis("localhost", 6379);
+//		jedis.auth("123456");
+//		String key = new StringBuilder("customerize").append(":").append(cust_schedule_id)
+//				.append(":").append("room").toString();
+//		Set<String> setString = jedis.smembers(key);
+//		
+//		if(setString == null || setString.size() == 0) {
+//			jedis.close();
+//			return null;
+//		}
+//		
+//		Set<MemberDetail> sets = new HashSet<MemberDetail>();
+//		for(String set : setString) {
+//			sets.add(gson.fromJson(set, MemberDetail.class));
+//		}
+//		jedis.close();
+//		return sets;
+//	}
 	
 //	沒有email資訊
 //	public static void setRoomMembers (String cust_schedule_id, String current_member_id) {
